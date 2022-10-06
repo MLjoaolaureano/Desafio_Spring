3e98018608a8f6569094b9da040de3ea1bb972df
emicarvalho <emily.carvalho@mercadolivre.com>
2022-10-06 13:47:23 -0300

    feat: filtrar cliente pelo estado

--- src/main/java/com/spring/desafio/controller/ClientController.java
+++ src/main/java/com/spring/desafio/controller/ClientController.java
@@ -5,6 +5,7 @@ import com.spring.desafio.service.IClientService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
@@ -24,5 +25,10 @@ public class ClientController {
     @GetMapping()
     public ResponseEntity<List<ClientResponseDTO>> getAllClients() throws IOException {
         return ResponseEntity.ok(this.clientService.getAll());
+    }
+
+    @GetMapping("/filter")
+    public ResponseEntity<List<ClientResponseDTO>> getAllClientsByState(@RequestParam("state") String state) throws IOException {
+        return ResponseEntity.ok(this.clientService.getAllClientsByState(state));
     }
 }
