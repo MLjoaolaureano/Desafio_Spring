3660ab583e48450fa746deff003bbbc60887909f
isanchesm <izabele.sanchesm@mercadolivre.com>
2022-10-07 16:27:00 -0300

    fix:Adicionando classe de excessão DuplicatedProductIdException

--- src/main/java/com/spring/desafio/exception/ClientIdAlreadyExistsException.java
+++ src/main/java/com/spring/desafio/exception/ClientIdAlreadyExistsException.java
@@ -1,7 +1,7 @@
 package com.spring.desafio.exception;
 
 /**
- * This class is used as an Exception when the {@link com.spring.desafio.entity.Cliente} already exists in some operation
+ * This class is used as an Exception when the {@link com.spring.desafio.entity.Client} Id already exists in some operation
  */
 public class ClientIdAlreadyExistsException extends Exception {
     public ClientIdAlreadyExistsException(String message) {
