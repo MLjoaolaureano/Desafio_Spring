7ce6676f5f96afd190fd86d7384bca10ab4f83ad
isanchesm <izabele.sanchesm@mercadolivre.com>
2022-10-07 17:02:14 -0300

    fix:Realizado ajuste na inclusão de produto com Id duplicado

--- src/main/java/com/spring/desafio/exception/DuplicatedProductIdException.java
+++ src/main/java/com/spring/desafio/exception/DuplicatedProductIdException.java
@@ -3,7 +3,7 @@ package com.spring.desafio.exception;
 import com.spring.desafio.entity.Product;
 
 /**
- * This class is used as an Exception when duplicated {@link Product} Id is pass in purchase operation
+ * This class is used as an Exception when duplicated {@link Product} Id is pass to perform operation
  */
 public class DuplicatedProductIdException extends Exception {
 
